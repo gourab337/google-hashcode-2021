35ef2e5fef087770f269d53dc5dea70e332571d2
Gourab <19bcs118@iiitdwd.ac.in>
2021-02-24 17:47:07 +0530

    Changes

--- Practice Problem 2021/solution.cpp
+++ Practice Problem 2021/solution.cpp
@@ -35,14 +35,37 @@ typedef double ld;
 
 int main()
 {
- int t;
- cin>>t;
- while(t--){
-   
-   
-    
-cout<<endl;
- }
+
+freopen("input/a_example", "r", stdin);    
+    freopen("input/a_example_out.txt", "w", stdout);
+ 
+    int M,T2,T3,T4;
+    int I[100];
+    int i,j;
+    string in[100][100];
+    char t;
+ 
+    scanf("%d %d %d %d",&M,&T2,&T3,&T4);    
+    for(i=0;i<M;i++) {
+        scanf("%d",&I[i]);
+        //scanf("%c",&t);
+        for(j=0;j<I[i];j++) {
+            cin >> in[i][j];
+            //scanf("%c",&t);
+        }      
+    }
+ 
+    /*printf("%d %d %d %d",M,T2,T3,T4);
+    printf("\n");
+    for(i=0;i<M;i++) {
+        printf("%d ",I[i]);
+        for(j=0;j<I[i];j++) {
+            cout << in[i][j] << " ";
+        }
+        printf("\n");  
+    }*/
+ 
+
 
 return 0;
 }
