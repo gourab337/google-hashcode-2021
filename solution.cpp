92e3578d6d28f40dd444398dd3709ed6113325c9
Gourab Chakraborty <19bcs118@iiitdwd.ac.in>
2021-02-24 17:16:55 +0530

    Practice round 2021

---
+++ solution.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <complex>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>  
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define ff first
+// #define ss second
+ 
+const double pi=3.1415926535897932384626433832;
+const long long mod=1e9+7;
+//long long primeset[5000000]={};
+//typedef complex<double> cd;
+typedef long long ll;
+typedef double ld;
+#define For(i,p,n,q) for(long long i = p ; i<n ;i+=q)
+#define FOR(i,p,n) for(long long i = p ; i<n ;i++)
+ 
+//-----------------------------------gourab337----------------------------------
+// a =97
+
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--){
+   
+   
+    
+cout<<endl;
+ }
+
+return 0;
+}
